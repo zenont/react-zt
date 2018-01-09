5e37a10bee53a0eb1289145e42b0d82652296395
Zenon Trujillo <zenontrujillo@zenon-macbook.local>
2018-01-08 22:40:29 -0800

    button icon

--- src/common/Button/Button.tsx
+++ src/common/Button/Button.tsx
@@ -1,14 +1,19 @@
 import React, { Props, StatelessComponent } from 'react'
 import FontAwesomeIcon, { IconSizeType } from '@fortawesome/react-fontawesome'
 import classnames from 'classnames'
-import { faSpinner } from '@fortawesome/fontawesome-free-solid'
+import { faParagraph, faSpinner } from '@fortawesome/fontawesome-free-solid'
 
 export interface IButtonProps extends Props<HTMLElement> {
 	size?: IconSizeType
 }
 
 export const Button: StatelessComponent<IButtonProps> = ({ children }) => (
-	<a className={classnames('zt-component-btn', 'default')}>{children}</a>
+	<a className={classnames('zt-component-btn', 'default')}>
+		<div className="content">
+			<FontAwesomeIcon icon={faParagraph} />
+			{children}
+		</div>
+	</a>
 )
 export { IconSizeType }
 export default Button
